09491a3d3f5fd8bc357b4b0d224c706da4f76ac1
abdulahi roble <abdulahi_2610@hotmail.com>
2021-11-09 15:04:54 +0100

    setup render

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+app.use(express.static("public"));
+
+const { createPage } = require("./render.js");
+
+const forside = createPage("forside/forside.html", {
+    title: "Portfolio | Velkommen"
+});
+
+
+app.get("/", (req, res) => {
+    res.send(forside);
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, (error) => {
+    console.log("Server is running on", PORT);
+});
