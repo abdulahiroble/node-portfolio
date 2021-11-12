2cf59a4ae95c27fc2c7ceeae9be4a16648750eca
abdulahi roble <abdulahi_2610@hotmail.com>
2021-11-12 21:04:12 +0100

    So far

--- app.js
+++ app.js
@@ -5,6 +5,24 @@ const path = require("path");
 const fs = require("fs");
 
 app.use(express.static("public"));
+const dirPath = path.join(__dirname, "public/pdfs");
+
+const files = fs.readdirSync(dirPath).map(name => {
+    return {
+        name: path.basename(name, ".pdf"),
+        url: `/pdfs/${name}`
+    };
+});
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.use(
+    express.static("public", {
+        setHeaders: (res, filepath) =>
+            res.attachment(`pdf-express-${path.basename(filepath)}`)
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -24,6 +42,7 @@ const forside = createPage("forside/forside.html", {
 const contactPage = createPage("contact/contact.html");
 const projectsPage = createPage("projects/projects.html");
 const cvPage = createPage("cv/cv.html");
+// const pdf = createPage("views/index.ejs");
 
 app.get("/", (req, res) => {
     res.send(forside);
@@ -40,6 +59,15 @@ app.get("/contact", (req, res) => {
 
 app.get("/cv", (req, res) => {
     res.send(cvPage);
+});
+
+app.get("/", (req, res) => {
+    res.render("index", { files });
+});
+
+app.get("/:file", (req, res) => {
+    const file = files.find(f => f.name === req.params.file);
+    res.render("index", { files, file });
 });
 
 const PORT = process.env.PORT || 3000;
